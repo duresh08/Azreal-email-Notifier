1746c592084088a5f795e6d68565acab81de81a2
duresh08 <88092980+duresh08@users.noreply.github.com>
2022-12-12 16:26:16 +0530

    Update Azreal.py

--- Azreal.py
+++ Azreal.py
@@ -1,4 +1,29 @@
 import streamlit as st
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from smtplib import SMTP
+import smtplib
+import sys
 
-password = st.secrets["password"]
-st.markdown(password)
+if st.button("Send Email):
+  password = st.secrets["password"]
+  Output_msg = "Test Message"
+  msg = MIMEMultipart()
+  msg['Subject'] = "Azreal Notification"
+  msg['From'] = 'dhruv.suresh2@gmail.com'
+  html = """\
+  <html>
+    <head></head>
+    <body>
+      {0}
+    </body>
+  </html>
+  """.format(Output_msg.to_html())
+  part1 = MIMEText(html, 'html')
+  msg.attach(part1)
+  server = smtplib.SMTP('smtp.gmail.com', 587)
+  server.starttls()
+  server.login('dhruv.suresh2@gmail.com', password)
+  server.sendmail(msg['From'], 'f20180884g@alumni.bits-pilani.ac.in' , msg.as_string())
+  server.close()
